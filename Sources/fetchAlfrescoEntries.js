221f9192cb239475dfaa136ca3812734eb1080a8
ahmedSadoun <ahmedsaadoun866@gmail.com>
2024-04-24 16:01:21 +0200

    added build entry path function

--- Sources/fetchAlfrescoEntries.js
+++ Sources/fetchAlfrescoEntries.js
@@ -30,6 +30,33 @@ async function fetchSubEntries(entry_id) {
 
   return res.data;
 }
+async function fetchEntryParent(entry_id) {
+  let subEntriesUrl = `${url}/${entry_id}/parents`;
+  const res = await axios.get(subEntriesUrl, {
+    auth: {
+      username,
+      password,
+    },
+  });
+  // console.log(res.data);
+  return res.data.list;
+}
+// fetchEntryParent("49d74a77-6575-48a3-834f-a6bef54bcd36");
+async function buildEntryPath(entry_id, path) {
+  let entryPath = path || [];
+  let res = await fetchEntryParent(entry_id);
+  // console.log(JSON.stringify(res.entries));
+  if (res.entries?.length > 0) {
+    entryPath.push(res.entries[0].entry);
+    if (res.entries[0].entry.parentId) {
+      await buildEntryPath(res.entries[0].entry.parentId, entryPath);
+    }
+  }
+  // console.log(JSON.stringify(entryPath));
+  return entryPath;
+}
 
-// fetchRootEntries();
+buildEntryPath("7a91cb8d-3298-4a0f-b86b-9d463738c95a").then((res) => {
+  console.log("Sssssssss", res);
+});
 export { fetchRootEntries, fetchSubEntries };
